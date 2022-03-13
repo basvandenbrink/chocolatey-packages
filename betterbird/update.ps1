bfad9c7133b189bd0be7d94a80020e8dde194840
Bas van den Brink <basvandenbrink@users.noreply.github.com>
2022-03-13 23:16:36 +0100

    Update script

--- betterbird/update.ps1
+++ betterbird/update.ps1
@@ -1,36 +1,27 @@
 ﻿import-module au
 
-$domain   = 'https://github.com'
-$releases = "$domain/LibreCAD/LibreCAD/releases/latest"
+$domain   = 'https://www.betterbird.eu'
+$releases = "$domain/downloads/index.php"
 
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-    }
-    ".\librecad.nuspec" = @{
-      "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
+      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
     }
   }
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
-
-  $re    = '\.exe$'
-  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
-
-  $version  = ($url -split '/' | select -Last 1 -Skip 1)
-
-  $releaseNotesUrl = "$domain/LibreCAD/LibreCAD/releases/tag/" + $version
+  $releases = Invoke-WebRequest $releases
+  $version = ($releases.Content | Select-String -Pattern '<h3>Table of Downloads for version ([\.\d]+(-[\w\d]+)) \(.*\)</h3>').Matches.Groups[1].Value
+  $url   = "/downloads/WindowsInstaller/betterbird-{0}.en-US.win64.installer.exe" -f $version
 
   @{
-    URL32 = $domain + $url
+    URL64 = $domain + $url
     Version = $version
-    ReleaseNotes = $releaseNotesUrl
   }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor 64
