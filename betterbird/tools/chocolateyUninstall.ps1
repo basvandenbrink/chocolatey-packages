2853a3d772e84d6f7a1c43f59d98dfde1fb18e5a
Bas van den Brink <basvandenbrink@users.noreply.github.com>
2022-03-13 22:35:23 +0100

    Improve uninstaller

--- betterbird/tools/chocolateyUninstall.ps1
+++ betterbird/tools/chocolateyUninstall.ps1
@@ -3,25 +3,28 @@ $ErrorActionPreference = 'Stop';
 $packageName = 'betterbird'
 
 $uninstalled = $false
-[array]$key = Get-UninstallRegistryKey -SoftwareName 'Betterbird'
+[array]$key = Get-UninstallRegistryKey -SoftwareName 'Betterbird*'
 
 if ($key.Count -eq 1) {
-  $key | % {
+  $key | ForEach-Object {
     $packageArgs = @{
-      packageName    = $packageName
-      fileType       = 'EXE'
-      silentArgs     = '-ms'
-      validExitCodes = @(0)
-      file           = "$($_.UninstallString.Trim('"'))"
+      packageName = $packageName
+      fileType    = 'exe'
+      silentArgs  = '-ms'
+      validExitCodes= @(0)
+      file          = "$($_.UninstallString.Trim('"'))"
     }
 
     Uninstall-ChocolateyPackage @packageArgs
+
+    Write-Warning "Auto Uninstaller may detect Mozilla Maintenance Service."
+    Write-Warning "This should not be uninstalled if any other Mozilla product is installed."
   }
 } elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
-  Write-Warning "$key.Count matches found!"
+  Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $_.DisplayName"}
+  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
