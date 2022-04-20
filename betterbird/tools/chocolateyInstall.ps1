03562a7d179af8a490775432f7695fd901c412b6
Bas van den Brink <basvandenbrink@users.noreply.github.com>
2022-04-20 05:34:58 +0000

    AU: 1 updated - betterbird [skip ci] https://gist.github.com/cf2abf8d848094bfaf2fa984a0c85745/c3135130535ec328e733049c44387b88097316b7

--- betterbird/tools/chocolateyInstall.ps1
+++ betterbird/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'Betterbird'
 
-  checksum       = '4840f23905a6b2223262db84505e81bc57bc32cbc4d7425a7f0b14c04bc4f418'
+  checksum       = '284321d6acfb70befe184a2ca050f846f41162527e0da83333a03c58ad62c45d'
   checksumType   = 'sha256'
-  url            = 'https://www.betterbird.eu/downloads/WindowsInstaller/betterbird-91.8.0-bb29.en-US.win64.installer.exe'
+  url            = 'https://www.betterbird.eu/downloads/WindowsInstaller/betterbird-91.8.1-bb30.en-US.win64.installer.exe'
 
   silentArgs     = '-ms'
   validExitCodes = @(0)
