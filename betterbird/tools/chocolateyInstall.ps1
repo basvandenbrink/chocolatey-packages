b466d26f01256efe988594746c88323d134f7d3d
Bas van den Brink <basvandenbrink@users.noreply.github.com>
2022-05-03 23:41:25 +0200

    Add support for downloading Betterbird version after becoming outdated

--- betterbird/tools/chocolateyInstall.ps1
+++ betterbird/tools/chocolateyInstall.ps1
@@ -1,5 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$fullVersion = '91.9.0-bb31'
+$urlPrefix = 'https://www.betterbird.eu/downloads/WindowsInstaller/'
+$downloadFileName = 'betterbird-{0}.en-US.win64.installer.exe' -f $fullVersion
+$url = "{0}{1}" -f $urlPrefix, $downloadFileName
+
+$wc = New-Object System.Net.WebClient
+$stream = $null
+
+try {
+  $stream = $wc.OpenRead($url)
+} catch {
+  Write-Host "It appears that a new version of Betterbird already has been released, using the archive URL."
+  $url = "{0}Previous/{1}" -f $urlPrefix, $downloadFileName
+} finally {
+  if ($null -ne $stream ){
+    $stream.Close()
+  }
+}
+
 $packageArgs = @{
   packageName    = 'betterbird'
   fileType       = 'exe'
@@ -7,7 +26,7 @@ $packageArgs = @{
 
   checksum       = '7b598b551968ae62910372920f08cf7967379fd8687ff93594451ef025223272'
   checksumType   = 'sha256'
-  url            = 'https://www.betterbird.eu/downloads/WindowsInstaller/betterbird-91.9.0-bb31.en-US.win64.installer.exe'
+  url            = $url
 
   silentArgs     = '-ms'
   validExitCodes = @(0)
--- betterbird/update.ps1
+++ betterbird/update.ps1
@@ -6,7 +6,7 @@ $releases = "$domain/downloads/index.php"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*\`$fullVersion\s*=\s*)('.*')"        = "`$1'$($Latest.FullVersion)'"
       "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
       "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
     }
@@ -18,12 +18,11 @@ function global:au_GetLatest {
   $version_matches = ($releases.Content | Select-String -Pattern '<h3>Table of Downloads for version (([\.\d]+)(-[\w\d]+)) \(.*\)</h3>').Matches
   $version_stripped = $version_matches.Groups[2]
   $version_full = $version_matches.Groups[1]
-  $url   = "/downloads/WindowsInstaller/betterbird-{0}.en-US.win64.installer.exe" -f $version_full
 
   @{
-    URL64 = $domain + $url
+    FullVersion = $version_full
     Version = $version_stripped
   }
 }
 
-update -ChecksumFor 64
+update -ChecksumFor 64 -NoCheckChocoVersion
