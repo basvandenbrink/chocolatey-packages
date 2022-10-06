2640e174d787f1114596855437bb99b4d380bee4
Bas van den Brink <basvandenbrink@users.noreply.github.com>
2022-10-06 23:25:26 +0000

    AU: 1 updated - betterbird [skip ci] https://gist.github.com/cf2abf8d848094bfaf2fa984a0c85745/7a83d576c97be62ba5a4129d6f7b3e84cf16fb84

--- betterbird/tools/chocolateyInstall.ps1
+++ betterbird/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Betterbird'
-$fullVersion = '102.3.1-bb18'
+$fullVersion = '102.3.2-bb19'
 
 $locale = 'en-US' #https://github.com/chocolatey/chocolatey-coreteampackages/issues/933
 $locale = GetLocale -localeFile "$toolsPath\LanguageChecksums.csv" -product $softwareName
