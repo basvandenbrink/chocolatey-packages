d06250928ea0b2e3242e114aad00663e61997afb
Bas van den Brink <2811984+basvandenbrink@users.noreply.github.com>
2025-01-13 21:41:14 +0100

    Add ART

---
+++ art/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = 'art'
+  fileType       = 'exe'
+  softwareName   = 'ART'
+
+  checksum64       = '9a819a985e8e3bcc7bce9b9f98e355a6a46c26878aae53c1bf60276ee89feda4'
+  checksumType64   = 'sha256'
+  url64            = 'https://github.com/artpixls/ART/releases/download/1.24.5/ART_1.24.5_Win64.exe'
+
+  silentArgs     = '/verysilent /allusers'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ art/update.ps1
@@ -0,0 +1,29 @@
+﻿Import-Module au
+
+$softwareName = 'ART'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+      "(?i)^(\s*softwareName\s*=\s*)'.*'"     = "`${1}'$softwareName'"
+    }
+    ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $LatestRelease = Get-GitHubRelease artpixls ART
+
+  @{
+    URL64        = $LatestRelease.assets | Where-Object {$_.name.EndsWith("Win64.exe")} | Select-Object -ExpandProperty browser_download_url
+    Version      = $LatestRelease.tag_name
+    ReleaseNotes = $LatestRelease.html_url
+  }
+}
+
+update -ChecksumFor 64
