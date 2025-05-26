7a1026385f6be2dbaf017ab6f603bb348e711884
Bas van den Brink <basvandenbrink@users.noreply.github.com>
2025-05-26 23:26:58 +0000

    AU: 2 updated - art betterbird [skip ci] https://gist.github.com/basvandenbrink/cf2abf8d848094bfaf2fa984a0c85745/41c8ff4057d6dacc59d90cb32148d583c6f15672

--- art/tools/chocolateyInstall.ps1
+++ art/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'ART'
 
-  checksum64       = 'cb8a220a4af5bb68f173cd9f39d325909c2b372738688515408a4fa1c7ce0d9e'
+  checksum64       = '8b9db33be142675cf544c0c676420186928e2ccec708f49b8f199a3e07fced1b'
   checksumType64   = 'sha256'
-  url64            = 'https://github.com/artpixls/ART/releases/download/1.25.4/ART_1.25.4_Win64.exe'
+  url64            = 'https://github.com/artpixls/ART/releases/download/1.25.5/ART_1.25.5_Win64.exe'
 
   silentArgs     = '/verysilent /allusers'
   validExitCodes = @(0)
