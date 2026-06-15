cddbf731e2a5c2f0c7850f02ee7e6fd0041b1aa1
Bas van den Brink <basvandenbrink@users.noreply.github.com>
2026-06-15 23:25:39 +0000

    AU: 1 updated - art [skip ci] https://gist.github.com/basvandenbrink/cf2abf8d848094bfaf2fa984a0c85745/659923bc67c0c0c97e8ebe285d13f74c76389aa9

--- art/tools/chocolateyInstall.ps1
+++ art/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'ART'
 
-  checksum64       = '879c56f28ed745c080840c329ed7abdd5eb2a264c2b97779e03fd64e84fff53d'
+  checksum64       = '325bf777140eeb46492be185d5c35b97dd20aa42f2e7a12ac863dc5d5b2fe948'
   checksumType64   = 'sha256'
-  url64            = 'https://github.com/artraweditor/ART/releases/download/1.26.5/ART_1.26.5_Win64.exe'
+  url64            = 'https://github.com/artraweditor/ART/releases/download/1.26.6/ART_1.26.6_Win64.exe'
 
   silentArgs     = '/verysilent /allusers'
   validExitCodes = @(0)
